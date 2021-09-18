712ec53073c8abe038a74cf42d035256f62d4a3e
risset <risset@mailbox.org>
2021-09-18 19:12:18 +0100

    Updated zsh configuration

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -13,12 +13,11 @@ alias grep='grep -i --color=auto'
 alias fgrep='fgrep -i --color=auto'
 alias egrep='egrep -i --color=auto'
 
-
 # Shell
 alias duh='sudo du -h --max-depth=1'
 
 # C++
-alias build='cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1 && mv compile_commands.json ..'
+alias cbuild='cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1 && mv compile_commands.json ..'
 
 # Applications
 alias e=emacsclient
@@ -28,11 +27,6 @@ alias tn='tmux new-session -s'
 alias ta='tmux attach-session -t'
 alias tk='tmux kill-session -t'
 alias untar='tar -zxvf'
-alias serve='python3 -m http.server'
-
-# Postgresql
-alias db='sudo -iu postgres psql'
-alias createdb='sudo -iu postgres createdb'
 
 # Git
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
